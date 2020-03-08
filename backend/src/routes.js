52c7f61a237737a3d7d3aad5b6c10d09ba2e8829
Artur Stefan <arthurstephan13@gmail.com>
2020-03-08 17:48:26 -0300

    Adiciona Gitignore

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const multer = require('multer');
-
 const uploadConfig =  require('./config/upload')
 
 const SessionController = require('./controllers/SessionController');
@@ -11,5 +10,6 @@ const upload = multer(uploadConfig);
 
 routes.post('/sessions',SessionController.store);
 
-routes.post('/spots',upload.single(thumbnail),SpotController.store);
+routes.post('/spots',upload.single('thumbnail'), SpotController.store);
+
 module.exports = routes;
