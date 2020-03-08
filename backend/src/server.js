fa3f954fc351dc7ab045fc735d5924dced1eb6be
Artur Stefan <arthurstephan13@gmail.com>
2020-03-08 16:32:09 -0300

    Criando arquivo de rotas

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const routes = require('./routes')
 
 const app = express();
 
-app.get('/', (req,res)=>{
-    return res.json({message:"hello world"});
-});
+app.use(express.json());
+
 
 app.listen(3333);
