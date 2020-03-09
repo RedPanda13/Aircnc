03875c6cc65b933990707632403642534deaa9ba
Artur Stefan <arthurstephan13@gmail.com>
2020-03-09 11:41:18 -0300

    Criando formulário para cadastro de Spots

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import camera from '../../assets/camera.svg';
+
+import './styles.css';
 
 export default function New() {
-    return <div />
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');
+    const [thumbnail, setThumbnail] = useState(null);
+
+   function handleSubmit() {
+
+   }
+   
+    return (
+        <form onSubmit = {handleSubmit}>
+            <label id="thumbnail">
+                <input type="file" onChange={event => setThumbnail(event.target.files)} />
+                <img src={camera} alt="Select img" />
+            </label>
+
+            <label htmlFor="company">EMPRESA *</label>
+            <input
+            id="company"
+            placeholder="Sua empresa incrivel"
+            value={company}
+            onChange={event => setCompany(event.target.value)}
+            />
+
+            <label htmlFor="techs">TECNOLOGIAS * <span>(separadas por virgula)</span></label>
+            <input
+            id="techs"
+            placeholder="Quais tecnologias usam?"
+            value={techs}
+            onChange={event => setTechs(event.target.value)}
+            />
+
+            <label htmlFor="techs">VALOR DA DIÁRIA * <span>(em branco para GRATUITO)</span></label>
+            <input
+            id="price"
+            placeholder="Valor cobrado por dia"
+            value={price}
+            onChange={event => setPrice(event.target.value)}
+            />
+
+            <button type="submit" className="btn">Cadastrar</button>
+
+        </form>
+    )
 }
