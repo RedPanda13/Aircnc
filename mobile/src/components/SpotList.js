37c659e139b18c15fd9ef3676b37bb237405ea16
Artur Stefan <arthurstephan13@gmail.com>
2020-03-10 17:11:23 -0300

    Exibindo os spots

--- mobile/src/components/SpotList.js
+++ mobile/src/components/SpotList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, FlatList, Image, TouchableOpacity } from 'react-native';
 
 import api from '../services/api';
 
@@ -21,7 +21,25 @@ export default function SpotList({ tech }) {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
+            <Text>Empresas que usam <Text style={styles.bold}>{ tech }</Text></Text>
+
+            <FlatList 
+                style={styles.list}
+                data={spots}
+                keyExtractor={spot => spot._id}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                renderItem={({ item }) => (
+                    <View style={styles.listItem}>
+                        <Image style={styles.thumbnail} source={{ uri: item.thumbnail_url }} />
+                        <Text style={styles.company}>{item.company}</Text>
+                        <Text style={styles.price}>{item.price ? `R$${item.price}/dia` : `GRATUITO`}</Text>
+                        <TouchableOpacity onPress={() => {}} style={styles.button}>
+                            <Text style={styles.buttonText}>Solicitar reserva</Text>
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     )
 }
@@ -40,5 +58,47 @@ const styles = StyleSheet.create({
 
     bold: {
         fontWeight: 'bold',
+    },
+
+    list: {
+        paddingHorizontal: 20,
+    },
+
+    listItem: {
+        marginRight: 15,
+    },
+
+    thumbnail: {
+        width: 200,
+        height: 120,
+        resizeMode: 'cover',
+        borderRadius: 2,
+    },
+
+    company: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 10,
+    },
+
+    price: {
+        fontSize: 15,
+        color: '#999',
+        marginTop: 5,
+    },
+
+    button: {
+        height: 32,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+        marginTop: 15,
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 15,
     }
 });
